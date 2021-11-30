d90cf763ba2855f2b2fae68f58737adf7ee84651
Clément Carré <clement.carre@bionomeex.com>
2021-11-30 18:14:00 +0100

    Upadte generate to takes args

--- Umitengu/Umitengu/Modules/MachineLearning.cs
+++ Umitengu/Umitengu/Modules/MachineLearning.cs
@@ -1,14 +1,43 @@
 ﻿using Discord.Commands;
 using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Umitengu.Modules
 {
     public class MachineLearning : ModuleBase
     {
+
+        public static bool _isBusy;
+
         [Command("Generate", RunMode = RunMode.Async)]
-        public async Task Generate()
+        public async Task Generate([Remainder] string args = "")
         {
-            await ReplyAsync("Hey");
+            if(_isBusy) {
+                await ReplyAsync("Already in use, please try later ...");
+                return;
+            }
+
+            _isBusy = true;
+
+            var msg = await ReplyAsync("generating : " + args);
+
+            ProcessStartInfo si = new()
+            {
+                Arguments = $"generate.py {args}",
+                WorkingDirectory = Program.Credentials.Path,
+                FileName = "python"
+            };
+
+            Console.WriteLine(args);
+
+            Process.Start(si).WaitForExit();
+            await msg.DeleteAsync();
+            await Context.Channel.SendFileAsync(Program.Credentials.Path + "/output.png");
+            File.Delete(Program.Credentials.Path + "/output.png");
+
+            _isBusy = false;
         }
     }
 }
