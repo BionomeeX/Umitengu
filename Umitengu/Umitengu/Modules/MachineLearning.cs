c4abfb3d4d1fb8bdab5aae988f960c9b53321ae9
Xwilarg <xwilarg@yahoo.fr>
2021-11-30 16:32:54 +0100

    Add empty Discord bot

---
+++ Umitengu/Umitengu/Modules/MachineLearning.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Umitengu.Modules
+{
+    public class MachineLearning : ModuleBase
+    {
+        [Command("Generate", RunMode = RunMode.Async)]
+        public async Task Generate()
+        {
+            await ReplyAsync("Hey");
+        }
+    }
+}
